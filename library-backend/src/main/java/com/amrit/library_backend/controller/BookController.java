015aca3d9320f14c51ca4ea83fb736d35458322e
amrit-verse <amriteshmishra729@gmail.com>
2026-05-13 22:06:23 +0530

    Added delete API

--- library-backend/src/main/java/com/amrit/library_backend/controller/BookController.java
+++ library-backend/src/main/java/com/amrit/library_backend/controller/BookController.java
@@ -26,4 +26,22 @@ public class BookController {
     public Book addBook(@RequestBody Book book) {
         return bookRepository.save(book);
     }
+
+    @DeleteMapping("/{id}")
+    public void deleteBook(@PathVariable Integer id) {
+        bookRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    public Book updateBook(@PathVariable Integer id, @RequestBody Book updatedBook) {
+
+        Book book = bookRepository.findById(id).orElseThrow();
+
+        book.setTitle(updatedBook.getTitle());
+        book.setAuthor(updatedBook.getAuthor());
+        book.setCategory(updatedBook.getCategory());
+        book.setAvailable(updatedBook.isAvailable());
+
+        return bookRepository.save(book);
+    }
 }
